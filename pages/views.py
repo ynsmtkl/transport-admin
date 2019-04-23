a9f479c4919fa4843e8ebf7ad92610f9fc3ff560
YNSMTKL <yns.mtkl@gmail.com>
2019-04-23 17:54:12 +0100

    add 1

--- pages/views.py
+++ pages/views.py
@@ -48,7 +48,16 @@ class UserLoginApiView(APIView):
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
-class UserRegisterView(CreateAPIView):
+class UserRegisterApiView(APIView):
+    permission_classes = [AllowAny]
     serializer_class = UserRegisterSerializer
-    queryset = User.objects.all()
+
+    def post(self, request):
+        data = request.data
+        serializer = UserRegisterSerializer(data=data)
+
+        if serializer.is_valid(raise_exception=True):
+            new_data = serializer.data
+            return Response(new_data, status=HTTP_200_OK)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
