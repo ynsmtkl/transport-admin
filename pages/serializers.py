82be070ad93c609f1775161406ec0e53829723c0
YNSMTKL <yns.mtkl@gmail.com>
2019-04-23 01:00:53 +0100

    test AA

--- pages/serializers.py
+++ pages/serializers.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
+from django.db.models import Q
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from .models import Connect
 
 
@@ -24,9 +27,34 @@ class UserLoginSerializer(serializers.ModelSerializer):
         ]
 
         extra_kwargs = {
-            "password": {
+            "password":{
                 "write_only": True
             }
         }
+
+        def validate(self, data):
+            user_obj = None
+            email = data.get("email")
+            username = data.get("username")
+            password = data['password']
+            if not email and not username:
+                raise ValidationError("Email or Username is required to login")
+            user = User.objects.filter(
+                Q(email=email) |
+                Q(username=username)
+            ).distinct()
+
+            if user.exists() and user.count == 1:
+                user_obj = user.first()
+            else:
+                raise ValidationError("This email or username is not valid")
+
+            if user_obj:
+                if not user_obj.check_password(password):
+                    raise ValidationError("password is not correct, please try again!")
+
+            data["token"] = "some random token"
+
+            return data
 
 
