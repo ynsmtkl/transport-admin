244cab38049b0815de9952be267afd19be0eef2f
YNSMTKL <yns.mtkl@gmail.com>
2019-04-23 16:41:17 +0100

    add u

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
@@ -46,16 +48,7 @@ class UserLoginApiView(APIView):
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
-class UserRegisterApiView(APIView):
-    permission_classes = [AllowAny]
+class UserRegisterApiView(CreateAPIView):
     serializer_class = UserRegisterSerializer
-
-    def post(self, request):
-        data = request.data
-        serializer = UserRegisterSerializer(data=data)
-
-        if serializer.is_valid(raise_exception=True):
-            new_data = serializer.data
-            return Response(new_data, status=HTTP_200_OK)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+    queryset = User.objects.all()
 
