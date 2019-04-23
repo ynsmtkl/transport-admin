e9e89feb4b7453fcceef65284c96ccfc476b7546
YNSMTKL <yns.mtkl@gmail.com>
2019-04-23 17:18:57 +0100

    register1

--- pages/urls.py
+++ pages/urls.py
@@ -4,9 +4,6 @@ from rest_framework import routers
 
 from . import views
 
-router = routers.DefaultRouter()
-router.register('users', views.UserRegisterSerializer)
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
@@ -16,6 +13,6 @@ urlpatterns = [
     url(r'^get_token$', views.get_token, name='token'),
 
     url(r'^login$', views.UserLoginApiView.as_view(), name='login'),
-    url('register/', views.UserRegisterSerializer.as_view(), name='register'),
+    url(r'^register$', views.UserRegisterView.as_view(), name='register'),
 
 ]
