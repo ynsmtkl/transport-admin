69ce5f7546fe29ca9e5983a747a31cfc521ecb5b
YNSMTKL <yns.mtkl@gmail.com>
2019-04-29 16:43:09 +0100

    add type of user to admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from api.users.models import Connect
+from api.users.models import Connect, UserType
 
 # Register your models here.
 admin.site.register(Connect)
+admin.site.register(UserType)
